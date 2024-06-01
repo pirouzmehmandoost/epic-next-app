592ba62b07d6dd0255ee0521f2c7f555096fabb9
Pirouz Mehmandoost <pirouzm@systemss-mbp.mynetworksettings.com>
2024-06-01 12:15:39 -0700

    add directory frontend- the entry point to next.js app

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="container mx-auto py-6">
+      <Button>Our Cool Button</Button>
+    </main>
+  );
+}
